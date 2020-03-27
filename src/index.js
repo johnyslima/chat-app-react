44dbef56c6fc234c25e731e5414894af0e31a24f
johnyslima <johnyslima@yandex.ru>
2020-03-28 04:58:38 +0500

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Login from './login'
+import SignUp from './signup'
+import Dashboard from './dashboard'
+
+const firebase = require('firebase')
+require('firebase/firestore')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCIW7urxJ4IYkyzVLiOuosp9YKxxXO9tQ0",
+  authDomain: "chat-app-by-johny-slima-react.firebaseapp.com",
+  databaseURL: "https://chat-app-by-johny-slima-react.firebaseio.com",
+  projectId: "chat-app-by-johny-slima-react",
+  storageBucket: "chat-app-by-johny-slima-react.appspot.com",
+  messagingSenderId: "388858873828",
+  appId: "1:388858873828:web:aa3e4e95c5e170013069f1",
+  measurementId: "G-W4R5CTHEE7"
+})
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+      <Route path='/login' component={Login}></Route>
+      <Route path='/signup' component={SignUp}></Route>
+      <Route path='/dashboard' component={Dashboard}></Route>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
---
+++ src/signup/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class SignUp extends React.Component {
+
+    render() {
+        return (
+            <h1>
+                SignUp page
+            </h1>
+        )
+    }
+}
+
+export default SignUp

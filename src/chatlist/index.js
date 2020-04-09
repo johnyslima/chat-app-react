150ecb48da7197a990286588c929366d051f8cd8
johnyslima <johnyslima@yandex.ru>
2020-04-10 00:50:24 +0500

    dashboard, chatComponent, eslint

--- src/chatlist/index.js
+++ src/chatlist/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import {
   List,
   ListItem,
@@ -8,19 +8,92 @@ import {
   Avatar,
   Typography,
   Button,
-} from "@material-ui/core"
-import withStyles from "@material-ui/core/styles/withStyles"
+  Divider,
+} from '@material-ui/core'
+import withStyles from '@material-ui/core/styles/withStyles'
 import NotificationImportant from '@material-ui/icons/NotificationImportant'
-import styles from "./styles"
+import styles from './styles'
 
 class ChatList extends React.Component {
 
+  newChat = () => {
+    console.log('this is a new chat')
+  }
+
+  selectChat = (index) => {
+    console.log(`select chat ${index}`)
+  }
+
   render() {
-    return(
-      <div>
-        <h1>qweqe</h1>
-      </div>
+
+    const {
+      classes,
+      chats,
+      selectedChatIndex,
+      userEmail
+    } = this.props
+
+    if (chats.length > 0) {
+      return (
+        <main className={classes.root}>
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            className={classes.newChatBtn}
+            onClick={this.newChat}>
+              New Message
+          </Button>
+          <List>
+            {
+              chats.map((_chat, _index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <div key={_index}>
+                  <ListItem
+                    onClick={() => this.selectChat(_index)}
+                    className={classes.listItem}
+                    selected={selectedChatIndex === _index}
+                    alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar
+                        alt="Remy Sharp">
+                        {_chat.users.filter((_user) => _user !== userEmail)[0].split('')[0]}
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={_chat.users.filter((_user) => _user !== userEmail)[0]}
+                      secondary={(
+                        <>
+                          <Typography component="span" color="textPrimary">
+                            {
+                              `${_chat.messages[_chat.messages.length - 1].message.substring(0, 30)} ...`
+                            }
+                          </Typography>
+                        </>
+                      )} />
+                  </ListItem>
+                  <Divider />
+                </div>
+              ))
+            }
+          </List>
+        </main>
+      )
+    }
+    return (
+      <main className={classes.root}>
+        <Button
+          variant="contained"
+          fullWidth
+          color="primary"
+          onClick={this.newChat}
+          className={classes.newChatBtn}>
+          New Message
+        </Button>
+      </main>
     )
+
+
   }
 }
 
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,15 +1,67 @@
-import React from "react";
-import ChatList from "../chatlist";
+import React from 'react'
+import ChatList from '../chatlist'
+
+const firebase = require('firebase')
 
 class Dashboard extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    }
+  }
+
+  newChatBtnClicked = () => {
+    this.setState({newChatFormVisible: true, selectedChat: null})
+  }
+
+  selectChat = (chatIndex) => {
+    console.log('selected chat', chatIndex)
+  }
+
+  componentDidMount = () => {
+    const { history } = this.props
+    firebase
+      .auth()
+      .onAuthStateChanged(async (_usr) => {
+        if (!_usr) {
+          history.push('/login')
+        } else {
+          await firebase
+            .firestore()
+            .collection('chats')
+            .where('users', 'array-contains', _usr.email)
+            .onSnapshot(async (res) => {
+              const chats = res.docs.map((_doc) => _doc.data())
+              await this.setState({
+                email: _usr.email,
+                chats
+              })
+              console.log(this.state)
+            })
+        }
+      })
+  }
+
   render() {
+    const { history } = this.props
+    const { chats, email, selectedChat } = this.state
     return (
       <div>
-        <h1>Dashboard page</h1>
-        <ChatList></ChatList>
+        <ChatList
+          history={history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={chats}
+          userEmail={email}
+          selectedChatIndex={selectedChat} />
       </div>
-    );
+    )
   }
 }
 
-export default Dashboard;
+export default Dashboard
